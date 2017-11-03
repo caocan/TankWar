a4e266bb41c495a145884d6a2d7b5aeb4bbe83f4
caocan <wwwcaocan@sina.cn>
2017-11-03 16:44:53 +0800

    TankWar0.1版本 产生了一个窗口，添加关闭事件，并且窗口大小设置不变

--- TankWar0.1/src/TankClient.java
+++ TankWar0.1/src/TankClient.java
@@ -1,10 +1,23 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame{
 
     public void lauchFrame(){
         this.setLocation(400, 300);
         this.setSize(800, 600);
+
+        //点X可以使窗口关闭
+        this.addWindowListener(new WindowAdapter() {    //添加窗口关闭事件
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        //窗口大小不变化
+        this.setResizable(false);
         setVisible(true);
     }
 
