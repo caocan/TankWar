af6eea01c9cd224d6cf60b63bdea7bc35ea26698
caocan <wwwcaocan@sina.cn>
2017-11-03 22:11:50 +0800

    TankWar0.9版本 重构键盘监听，添加键盘按键抬起来的处理方法，使得Tank真正能够在八个方向上行走

---
+++ TankWar0.9/src/TankClient.java
@@ -0,0 +1,98 @@
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankClient extends Frame{
+
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
+
+    Tank myTank = new Tank(50, 50);
+
+    //虚拟的图片
+    Image offScreenImage = null;
+
+    //画出红色的坦克，窗口重画的时候自动调用
+    public void paint(Graphics g){
+        myTank.draw(g);
+    }
+
+    //解决闪烁的问题
+    public void update(Graphics g){
+        //创建虚拟图片
+        if(offScreenImage == null){
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+
+        //拿到画笔
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.GREEN);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
+    }
+
+    public void lauchFrame(){
+        this.setLocation(400, 300);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
+
+        //点X可以使窗口关闭
+        this.addWindowListener(new WindowAdapter() {    //添加窗口关闭事件
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        //窗口大小不变化
+        this.setResizable(false);
+        this.setBackground(Color.GREEN);
+
+        //创建用于监听键盘事件的监听器
+        this.addKeyListener(new KeyMonitor());
+        setVisible(true);
+
+        new Thread(new PaintThread()).start();
+    }
+
+
+    public static void main(String[] args) {
+        TankClient tc = new TankClient();
+        tc.lauchFrame();
+    }
+
+    //写一个内部类，用来重绘我们的窗口，使坦克动起来
+    private class PaintThread implements Runnable{
+        @Override
+        public void run() {
+            while(true){
+                repaint();
+                try {
+                    //每个50ms，窗口就重绘一次，坦克就移动一次
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //用于监听键盘事件的类
+    private class KeyMonitor extends KeyAdapter{
+
+        //使坦克可以在键盘控制下向四个方向移动
+        public void keyPressed(KeyEvent e){
+            myTank.keyPressed(e);
+        }
+
+        //处理键盘抬起来时的情况
+        @Override
+        public void keyReleased(KeyEvent e) {
+            myTank.keyReleased(e);
+        }
+    }
+}
