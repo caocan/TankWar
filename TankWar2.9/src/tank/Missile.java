5059068cb6cc72397fcc33ec6be5f9d5c11526e3
caocan <wwwcaocan@sina.cn>
2017-11-07 13:57:41 +0800

    TankWar2.9版本 增加配置文件。注意配置文件没有空格

---
+++ TankWar2.9/src/tank/Missile.java
@@ -0,0 +1,202 @@
+package tank;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Missile {
+
+    public static final int XSPEED = 10;
+    public static final int YSPEED = 10;
+
+    public static final int WIDTH = 10;
+    public static final int HEIGHT = 10;
+
+    int x, y;
+    Direction dir;
+
+    private TankClient tc;
+
+    private boolean good;
+
+    //live的getter()
+    public boolean isLive() {
+        return live;
+    }
+
+    private boolean live = true;
+
+    public Missile(int x, int y, Direction dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public Missile(int x, int y, boolean good, Direction dir, TankClient tc){
+        this(x, y, dir);
+        this.good = good;
+        this.tc = tc;
+    }
+
+    public void draw(Graphics g) {
+
+        if(!live){
+            //让爆炸消失
+            tc.missiles.remove(this);
+            return;
+        }
+
+
+//        Color c = g.getColor();
+//        if(good) g.setColor(Color.RED);
+//        else g.setColor(Color.BLACK);
+//        g.fillOval(x, y, WIDTH, HEIGHT);
+//        g.setColor(c);
+
+        switch (dir){
+            case L:
+//                g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x, y + Tank.HEIGHT/2);
+                g.drawImage(imgs.get("L"),x,y,null);
+                break;
+            case LU:
+                g.drawImage(imgs.get("LU"),x,y,null);
+                break;
+            case U:
+                g.drawImage(imgs.get("U"),x,y,null);
+                break;
+            case RU:
+                g.drawImage(imgs.get("RU"),x,y,null);
+                break;
+            case R:
+                g.drawImage(imgs.get("R"),x,y,null);
+                break;
+            case RD:
+                g.drawImage(imgs.get("RD"),x,y,null);
+                break;
+            case D:
+                g.drawImage(imgs.get("D"),x,y,null);
+                break;
+            case LD:
+                g.drawImage(imgs.get("LD"),x,y,null);
+                break;
+        }
+
+        move();
+    }
+
+    private static Toolkit tk = Toolkit.getDefaultToolkit();
+
+    //将图片设置为静态,从ClassPath下取资源
+    private static Image[] missileImages = null;
+
+    private static Map<String, Image> imgs = new HashMap<String, Image>();
+
+    static
+    {
+        //比直接初始化更好，因为可以放代码，可以进行调试
+        missileImages = new Image[]{
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileL.gif")),
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileLU.gif")),
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileU.gif")),
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileRU.gif")),
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileR.gif")),
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileRD.gif")),
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileD.gif")),
+                tk.getImage(Tank.class.getClassLoader().getResource("images/missileLD.gif"))
+        };
+//        System.out.println("ok");
+        imgs.put("L",missileImages[0]);
+        imgs.put("LU",missileImages[1]);
+        imgs.put("U",missileImages[2]);
+        imgs.put("RU",missileImages[3]);
+        imgs.put("R",missileImages[4]);
+        imgs.put("RD",missileImages[5]);
+        imgs.put("D",missileImages[6]);
+        imgs.put("LD",missileImages[7]);
+
+    }
+
+    private void move(){
+
+        switch (dir) {
+            case L:
+                x -= XSPEED;
+                break;
+            case LU:
+                x -= XSPEED;
+                y -= YSPEED;
+                break;
+            case U:
+                y -= YSPEED;
+                break;
+            case RU:
+                x += XSPEED;
+                y -= YSPEED;
+                break;
+            case R:
+                x += XSPEED;
+                break;
+            case RD:
+                x += XSPEED;
+                y += YSPEED;
+                break;
+            case D:
+                y += YSPEED;
+                break;
+            case LD:
+                x -= XSPEED;
+                y += YSPEED;
+                break;
+            case STOP:
+                break;
+        }
+
+        if(x < 0 || y < 0 || x > TankClient.GAME_WIDTH || y > TankClient.GAME_HEIGHT){
+            live = false;
+
+        }
+
+
+    }
+
+    public Rectangle getRect(){
+        return new Rectangle(x, y, WIDTH, HEIGHT);
+    }
+
+    //可以通过观察者模式来重构这个问题
+    public boolean hitTank(Tank t){
+        if(this.live && this.getRect().intersects(t.getRect()) && t.isLive() && this.good != t.isGood()){
+            if(t.isGood()){
+                t.setLife(t.getLife() - 20);
+                if(t.getLife() <= 0) t.setLive(false);
+            }else{
+                t.setLive(false);
+            }
+
+            this.live = false;
+            Explode e = new Explode(x, y, tc);
+            tc.explodes.add(e);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean hitTanks(List<Tank> tanks){
+        for(int i = 0; i < tanks.size(); i++){
+            if(hitTank(tanks.get(i)))
+                return true;
+        }
+        return false;
+    }
+
+    //子弹撞墙的函数
+    public boolean hitWall(Wall w){
+        if(this.live && this.getRect().intersects(w.getRect())){
+            this.live = false;
+            return true;
+        }
+        return false;
+    }
+}
